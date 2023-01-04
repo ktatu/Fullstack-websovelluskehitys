382499c9cf92679581a6b9a3f180ee8059c21616
ktatu <kaikkonentatu@gmail.com>
2023-01-04 21:38:33 +0200

    5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Blog = ({blog}) => {
-  return (
-    <div>
-        {blog.title} {blog.author}
-    </div>  
-  )
+    const [expandedBlogViewState, setExpandedBlogViewState] = useState(false)
+
+    const handleBlogView = (event) => {
+        setExpandedBlogViewState(!expandedBlogViewState)
+    }
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        paddingBottom: 5,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    const MinimalBlogView = () => {
+        return (
+            <div>
+                {blog.title} {blog.author} <button onClick={handleBlogView}>View</button>
+            </div>
+        )
+    }
+
+    const ExpandedBlogView = () => {
+        return (
+            <div>
+                {blog.title} {blog.author} <button onClick={handleBlogView}>Hide</button>
+                <br />
+                {blog.url}
+                <br />
+                likes {blog.likes} <button>like</button>
+                <br />
+                {blog.author}
+            </div>
+        )
+    }
+
+    return (
+        <div style={blogStyle}>
+            {expandedBlogViewState
+                ? <ExpandedBlogView />
+                : <MinimalBlogView />
+            }
+        </div>
+    )
 }
 
 export default Blog
