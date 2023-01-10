73936b353e91312e73f94aca2c70a3fdd5aadb81
ktatu <kaikkonentatu@gmail.com>
2023-01-11 01:42:07 +0200

    5.19

---
+++ osa5/bloglist-frontend/cypress/e2e/blogs.cy.js
@@ -0,0 +1,53 @@
+describe("Blog app", function() {
+
+    const cypressUser = {
+        username: "cypressUser",
+        name: "cypressName",
+        password: "cypressPassword"
+    }
+
+    const cypressBlog = {
+        author: "Cypress",
+        title: "All about Cypress",
+        url: "https://example.com"
+    }
+
+    describe("When logged in", function() {
+        beforeEach(function() {
+            cy.request("POST", "http://localhost:3003/api/testing/reset")
+            cy.request("POST", "http://localhost:3003/api/users", cypressUser)
+            cy
+                .request("POST", "http://localhost:3003/api/login", { username: cypressUser.username, password: cypressUser.password })
+                .then((res) => {
+                    localStorage.setItem("loggedUser", JSON.stringify(res.body))
+                    cy.visit("http://localhost:3000")
+                })
+
+        })
+
+        it("A blog can be created", function() {
+            cy
+                .contains("new blog")
+                .click()
+
+            cy
+                .get("#url-input")
+                .type(cypressBlog.url)
+
+            cy
+                .get("#author-input")
+                .type(cypressBlog.author)
+
+            cy
+                .get("#title-input")
+                .type(cypressBlog.title)
+
+            cy
+                .get("#blog-submit")
+                .click()
+
+            cy.contains(`${cypressBlog.title} by ${cypressBlog.author} added`)
+        })
+    })
+
+})
