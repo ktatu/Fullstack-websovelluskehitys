f6328ca7ee31eea2269143792d47b9caed6d39e1
ktatu <kaikkonentatu@gmail.com>
2023-01-17 18:34:24 +0200

    7.4

--- osa7/routed-anecdotes/src/pages/CreateNew.js
+++ osa7/routed-anecdotes/src/pages/CreateNew.js
@@ -1,22 +1,22 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { useField } from "../hooks"
 
 const CreateNew = ({ addNew, addNotification }) => {
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
     const navigate = useNavigate()
-  
+    const content = useField("text")
+    const author = useField("text")
+    const info = useField("text")
   
     const handleSubmit = (e) => {
       e.preventDefault()
       addNew({
-        content,
-        author,
-        info,
+        content: content.value,
+        author: author.value,
+        info: info.value,
         votes: 0
       })
-      addNotification("a new anecdote " + content + " created!")
+      addNotification("a new anecdote " + content.value + " created!")
       navigate("/")
     }
   
@@ -26,15 +26,15 @@ const CreateNew = ({ addNew, addNotification }) => {
         <form onSubmit={handleSubmit}>
           <div>
             content
-            <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+            <input {...content} />
           </div>
           <div>
             author
-            <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+            <input {...author} />
           </div>
           <div>
             url for more info
-            <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+            <input {...info} />
           </div>
           <button>create</button>
         </form>
